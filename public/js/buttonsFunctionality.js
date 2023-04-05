ec9b76bf2d1d647aa56a0d0954da528fb315b232
Luis Morales <david.laleala@gmail.com>
2023-04-06 00:39:27 +0100

    feat(navigation): redesign navigation bar for pathfinding visualizer

--- public/js/buttonsFunctionality.js
+++ public/js/buttonsFunctionality.js
@@ -6,6 +6,41 @@ import { updateVelocity } from "./creator.js";
 import { useDijkstras } from "./algos/dijkstras.js";
 import { useAstar } from "./algos/astar/astar.js";
 // import { useGreedyBfs } from "./algos/greedyBFS.js";
+
+let selectedAlgorithm = {
+  "dfs-btn": false,
+  "dijkstra-btn": false,
+  "bfs-btn": false,
+  "astar-btn": false,
+  "greedyBfs-btn": false,
+};
+
+function updateSelectedAlgo(algo) {
+  for (const key in selectedAlgorithm) {
+    if (key != algo) {
+      selectedAlgorithm[key] = false;
+    }
+  }
+  selectedAlgorithm[algo] = true;
+}
+function toggleDropdown() {
+  document.getElementById("algo-dropdown").classList.toggle("show");
+}
+
+document.addEventListener("click", function (event) {
+  if (event.target.closest(".nav-icon")) {
+    toggleDropdown();
+  } else if (!event.target.closest(".dropdown")) {
+    var dropdowns = document.getElementsByClassName("dropdown");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+});
+
 document
   .getElementById("myWidth")
   .addEventListener("change", updateWidthAndHeight);
@@ -17,39 +52,49 @@ document.getElementById("myVelocity").addEventListener("change", () => {
   updateVelocity(Number(document.getElementById("myVelocity").value));
 });
 
-document.getElementById("random-target-btn").addEventListener("click", () => {
-  tableFunctionality.generateNewRandomTargets();
-});
-
 document.getElementById("grid").addEventListener("click", (e) => {
   tableFunctionality.colorTd(e);
 });
 
 document.getElementById("dfs-btn").addEventListener("click", () => {
-  const start = document.getElementsByClassName("start")[0].id;
-  const end = document.getElementsByClassName("end")[0].id;
-  useDepthFirstTraversal(start, end);
+  updateSelectedAlgo("dfs-btn");
 });
 document.getElementById("dijkstra-btn").addEventListener("click", () => {
-  const start = document.getElementsByClassName("start")[0].id;
-  const end = document.getElementsByClassName("end")[0].id;
-  useDijkstras(start, end);
+  updateSelectedAlgo("dijkstra-btn");
 });
 document.getElementById("bfs-btn").addEventListener("click", () => {
-  const start = document.getElementsByClassName("start")[0].id;
-  const end = document.getElementsByClassName("end")[0].id;
-  useDijkstras(start, end);
+  updateSelectedAlgo("bfs-btn");
 });
 document.getElementById("astar-btn").addEventListener("click", () => {
-  const start = document.getElementsByClassName("start")[0].id;
-  const end = document.getElementsByClassName("end")[0].id;
-
-  useAstar(start, end);
+  updateSelectedAlgo("astar-btn");
+});
+document.getElementById("greedyBfs-btn").addEventListener("click", () => {
+  updateSelectedAlgo("greedyBfs-btn");
 });
 
-// document.getElementById("greedyBfs-btn").addEventListener("click", () => {
-//   const start = document.getElementsByClassName("start")[0].id;
-//   const end = document.getElementsByClassName("end")[0].id;
+document.getElementById("runButton").addEventListener("click", () => {
+  let algoSelected = Object.keys(selectedAlgorithm).find(
+    (key) => selectedAlgorithm[key] === true
+  );
+  if (!algoSelected) {
+    alert("Select an algorithm first");
+  }
+  runAlgorithm(algoSelected);
+});
 
-//   useGreedyBfs(start, end);
-// });
+function runAlgorithm(algo) {
+  const start = document.getElementsByClassName("start")[0].id;
+  const end = document.getElementsByClassName("end")[0].id;
+  if (algo == "dfs-btn") {
+    useDepthFirstTraversal(start, end);
+  } else if (algo == "dijkstra-btn") {
+    useDijkstras(start, end);
+  } else if (algo == "bfs-btn") {
+    // NEED TO CHANGE TO BFS instead
+    useDijkstras(start, end);
+  } else if (algo == "astar-btn") {
+    useAstar(start, end);
+  } else if (algo == "greedyBfs-btn") {
+    // development
+  }
+}
