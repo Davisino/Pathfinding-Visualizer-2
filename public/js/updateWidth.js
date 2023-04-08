f3c49c1db8c7583a178771f8d2d3e5500419c4ae
Luis Morales <david.laleala@gmail.com>
2023-04-08 19:44:44 +0100

    update

--- public/js/updateWidth.js
+++ public/js/updateWidth.js
@@ -4,10 +4,7 @@ import * as creator from "./creator.js";
 export function updateWidthAndHeight() {
   const sliderWidth = document.getElementById("myWidth").value;
   const sliderHeight = document.getElementById("myHeight").value;
-  console.log(sliderWidth, sliderHeight);
-  // erase current table.
   document.getElementById("grid").innerHTML = "";
   creator.generateTable(Number(sliderWidth), Number(sliderHeight));
   Astar.generateAstarTable(Number(sliderWidth), Number(sliderHeight));
-  // UPDATE ASTAR TABLE
 }
